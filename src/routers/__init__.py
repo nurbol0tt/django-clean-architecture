dfcba7a6d54ad40902a736354edbda9947608974
nurbol0tt <fairtail764@gmail.com>
2023-08-02 23:02:26 +0600

    creat routers

---
+++ src/routers/__init__.py
@@ -0,0 +1,17 @@
+from .main_router import urlpatterns as main
+from .yasg_router import urlpatterns as yasg
+
+from django.conf import settings
+from django.contrib import admin
+
+urlpatterns = main + yasg
+
+admin.site.site_header = "Custom Admin"
+admin.site.site_title = "Custom Admin"
+admin.site.index_title = "Welcome to Custom Admin Panel"
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
